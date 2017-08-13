3f555c61af3a6bf3bd9ef0f59f82fb845390fbbc
Daniel Walden <Ariel@ACA8018A.ipt.aol.com>
2017-08-13 14:09:15 -0500

    added http req handlers

--- short-lnk/imports/api/users.js
+++ short-lnk/imports/api/users.js
@@ -7,16 +7,13 @@ import { Accounts } from 'meteor/accounts-base';
 Accounts.validateNewUser((user) => {
     const email = user.emails[0].address;
 
-    try {
-        new SimpleSchema({
+
+    new SimpleSchema({
         email: {
             type: String,
             regEx: SimpleSchema.RegEx.Email
         }
-        }).validate({ email });          
-    } catch (e) {
-        throw new Meteor.Error(400, e.message)
-    }
+    }).validate({ email });          
 
     return true;
 });
--- short-lnk/server/main.js
+++ short-lnk/server/main.js
@@ -1,10 +1,39 @@
 import { Meteor } from 'meteor/meteor';
-
+import { WebApp } from 'meteor/webapp';
 
 import '../imports/api/users';
-import '../imports/api/links';
+import { Links } from '../imports/api/links';
+import '../imports/startup/simple-schema-configuration.js';
 
 Meteor.startup(() => {
 
+    WebApp.connectHandlers.use((req, res, next) => {
+        const _id = req.url.slice(1);
+        const link = Links.findOne({ _id });
 
+        if (link) {
+            res.statusCode = 302;
+            res.setHeader('Location', link.url);
+            res.end();
+        } else {
+            next();
+        }
+
+    });
+    //Reference for middleware
+    // WebApp.connectHandlers.use((req, res, next) => {
+    //     console.log('This is from my custon middleware!!!');
+    //     console.log(req.url, req.method, req.headers, req.query);
+    //     //Set HTTP status code
+    //     // res.statusCode = 404;
+    //     //set HTTP header on the response
+    //     // res.setHeader('my-custom-header', 'here is dannys custom header');
+    //     //set HTTP body
+    //     // res.write('<h1>This is my middleware at work</h1>');
+    //     //End HTTP request
+    //     // res.end();
+
+
+    //     next();
+    // });
 });
